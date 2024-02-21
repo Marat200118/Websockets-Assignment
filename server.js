26ddf297ca1254fb07f9b8be63456bdc9dea0b32
Marat200118 <marat.samigullin1@gmail.com>
2024-02-21 20:45:33 +0100

    removed unnesesary code

--- server.js
+++ server.js
@@ -7,26 +7,6 @@ const io = new Server(server);
 const port = process.env.PORT || 80;
 
 const clients = {};
-// io.on("connection", (socket) => {
-//   clients[socket.id] = { id: socket.id };
-//   console.log("Socket connected", socket.id);
-
-//   clients[socket.id].x = 0;
-//   clients[socket.id].y = 0;
-
-//   socket.on("update", (targetSocketId, data) => {
-//     if (!clients[targetSocketId]) {
-//       return;
-//     }
-//     clients[socket.id].x = data.x;
-//     clients[socket.id].y = data.y;
-//     io.to(targetSocketId).emit("update", data);
-//   });
-
-//   socket.on("disconnect", () => {
-//     delete clients[socket.id];
-//   });
-// });
 
 io.on("connection", (socket) => {
   clients[socket.id] = { id: socket.id };
